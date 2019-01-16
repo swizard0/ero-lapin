1a3a95d403498af67585b39597301d38438304b9
Alexey Voznyuk <me@swizard.info>
2019-01-16 12:47:06 +0300

    lode skeleton impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,26 @@
-
 use futures::{
     Future,
     future::{
         lazy,
         result,
         Either,
+    },
+};
+
+use tokio::net::TcpStream;
+
+use lapin_futures::{
+    client::{
+        self,
+        ConnectionOptions,
+    },
+    channel::{
+        Channel,
+        BasicQosOptions,
+    },
+    types::{
+        FieldTable,
+        AMQPValue,
     },
 };
 
@@ -37,4 +53,77 @@ impl Default for AmqpParams {
 pub struct Params<N> {
     pub amqp_params: AmqpParams,
     pub lode_params: lode::Params<N>,
+}
+
+pub fn spawn<N>(
+    executor: &tokio::runtime::TaskExecutor,
+    params: Params<N>,
+    tcp_lode: Lode<TcpStream>,
+)
+    -> Lode<Channel<TcpStream>>
+where N: AsRef<str> + Send + 'static,
+{
+    let Params { amqp_params, lode_params, } = params;
+
+    let init_state = InitState {
+        amqp_params,
+        tcp_lode,
+        executor: executor.clone(),
+    };
+
+    lode::shared::spawn(
+        executor,
+        lode_params,
+        init_state,
+        init,
+        aquire,
+        release,
+        close,
+    )
+}
+
+struct InitState {
+    amqp_params: AmqpParams,
+    tcp_lode: Lode<TcpStream>,
+    executor: tokio::runtime::TaskExecutor,
+}
+
+struct AmqpConnected {
+    init_state: InitState,
+}
+
+fn init(
+    init_state: InitState,
+)
+    -> impl Future<Item = AmqpConnected, Error = ErrorSeverity<InitState, ()>>
+{
+
+    result(Err(ErrorSeverity::Fatal(())))
+}
+
+fn aquire(
+    connected: AmqpConnected,
+)
+    -> impl Future<Item = (Channel<TcpStream>, AmqpConnected), Error = ErrorSeverity<InitState, ()>>
+{
+
+    result(Err(ErrorSeverity::Fatal(())))
+}
+
+fn release(
+    connected: AmqpConnected,
+    _maybe_session: Option<Channel<TcpStream>>,
+)
+    -> impl Future<Item = AmqpConnected, Error = ErrorSeverity<InitState, ()>>
+{
+
+    result(Err(ErrorSeverity::Fatal(())))
+}
+
+fn close(
+    connected: AmqpConnected,
+)
+    -> impl Future<Item = InitState, Error = ()>
+{
+    result(Ok(connected.init_state))
 }
